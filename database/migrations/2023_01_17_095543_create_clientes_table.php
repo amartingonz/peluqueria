ce91b5ad226989ddf552bda4d632f8ad0f0c55d3
Alejandro <99648035+amartingonz@users.noreply.github.com>
2023-01-17 12:31:02 +0100

    Initial commit

---
+++ database/migrations/2023_01_17_095543_create_clientes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();   //  Clave primaria citas
+            $table->string("nombre",255);
+            $table->string("apellidos",255);
+            $table->string("telefono",255);
+            $table->string("email",255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
---
+++ database/migrations/2023_01_17_095748_create_citas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('citas', function (Blueprint $table) {
+            $table->id();   //  Clave primaria citas
+            $table->unsignedBigInteger('usuario_id');
+            $table->foreign('usuario_id')->references('id')->on('clientes');
+            $table->string('hora',255);
+            $table->date('fecha',255);
+            $table->string('anotacion',255);
+            $table->string('realizado',255); 
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('citas');
+    }
+};
